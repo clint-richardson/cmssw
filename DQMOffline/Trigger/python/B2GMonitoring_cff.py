bfc228bf78892c758fe77dff1e80b1dd29438c39
Clint <clint.a.richardson@gmail.com>
2017-06-14 22:47:23 +0200

    first attempt, the dqm using top studies is not necessarily correct

--- DQMOffline/Trigger/python/B2GMonitoring_cff.py
+++ DQMOffline/Trigger/python/B2GMonitoring_cff.py
@@ -1,16 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.HLTEGTnPMonitor_cfi import egmGsfElectronIDsForDQM,egHLTDQMOfflineTnPSource
+from DQMOffline.Trigger.topDiLeptonHLTEventDQM_cfi import topDiLeptonHLTOfflineDQM
 
-b2gDileptonHLTDQMOfflineTnPSource = egHLTDQMOFflineTnPSource.clone()
-b2gDileptonHLTDQMOfflineTnPSource.tagAndProbeCollections = cms.VPSet(
+b2gDielectronHLTDQMOfflineTnPSource = egHLTDQMOFflineTnPSource.clone()
+b2gDielectronHLTDQMOfflineTnPSource.tagAndProbeCollections = cms.VPSet(
          cms.PSet( 
             tagAndProbeConfigEle27WPTight,
             histConfigs = egammaStdHistConfigs,
             baseHistName = cms.string("eleWPTightTag_"),
             filterConfigs = cms.VPSet(
             cms.PSet(
-                folderName = cms.string("HLT/EGTagAndProbeEffs/HLT_DoubleEle37_27_CaloIdL_GsfTrkIdVL"),
+                folderName = cms.string("HLT/B2GHLTOffline/Dileptonic/HLT_DoubleEle37_27_CaloIdL_GsfTrkIdVL"),
                 rangeCuts = cms.VPSet(etRangeCut.clone(allowedRanges=cms.vstring("40:99999")),),
                 filterName = cms.string("hltDiEle27CaloIdLGsfTrkIdVLDPhiUnseededFilter"),
                 histTitle = cms.string(""),
@@ -20,7 +21,26 @@ b2gDileptonHLTDQMOfflineTnPSource.tagAndProbeCollections = cms.VPSet(
         ),
 )
 
+b2gDileptonHLTOfflineDQM = topDiLeptonHLTOfflineDQM.clone()
+b2gDileptonHLTOfflineDQM.setup.directory = cms.string('HLT/B2GHLTOffline/Dileptonic/CrossTriggers')
+b2gDileptonHLTOfflineDQM.setup.triggerExtras.pathsELECMU = cms.vstring(['HLT_Mu37_Ele27_CaloIdL_GsfTrkIdVL_v','HLT_Mu27_Ele37_CaloIdL_GsfTrkIdVL_v'])
+b2gDileptonHLTOfflineDQM.setup.triggerExtras.pathsDIMUON = cms.vstring([''])
+b2gDileptonHLTOfflineDQM.setup.triggerExtras.pathsDIELEC = cms.vstring([''])
+b2gDileptonHLTOfflineDQM.preselection.trigger.select = cms.vstring(['HLT_Mu37_Ele27_CaloIdL_GsfTrkIdVL_v','HLT_Mu27_Ele37_CaloIdL_GsfTrkIdVL_v'])
+
+b2gDimuonHLTOfflineDQM = topDiLeptonHLTOfflineDQM.clone()
+b2gDimuonHLTOfflineDQM.setup.directory = cms.string('HLT/B2GHLTOffline/Dileptonic/Dimuon')
+b2gDimuonHLTOfflineDQM.setup.triggerExtras.pathsELECMU = cms.vstring([''])
+b2gDimuonHLTOfflineDQM.setup.triggerExtras.pathsDIMUON = cms.vstring(['HLT_Mu30_TkMu11_v'])
+b2gDimuonHLTOfflineDQM.setup.triggerExtras.pathsDIELEC = cms.vstring([''])
+b2gDimuonHLTOfflineDQM.preselection.trigger.select = cms.vstring(['HLT_Mu30_TkMu11'])
+
+
+
 b2gMonitorHLT = cms.Sequence(
     egmGsfElectronIDsForDQM*
-    b2gDileptonHLTDQMOfflineTnPSource
+    b2gDielectronHLTDQMOfflineTnPSource*
+    b2gDileptonHLTOfflineDQM*
+    b2gDimuonHLTOfflineDQM
+
 )
